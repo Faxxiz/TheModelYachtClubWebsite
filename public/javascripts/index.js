d69cc4f6f2f27a25f3ab07c1d686f0bab7a746d6
Charles Castelain <charles.castelain@epitech.eu>
2020-06-26 09:13:39 +0200

    public resources added, header logo integrated

---
+++ public/javascripts/index.js
@@ -0,0 +1,9 @@
+/*const headerHeight = $(document).height();
+
+console.log(headerHeight + " AH");*/
+
+$(document).ready(function() {
+
+    $(".home-content").css("margin-top", "208px");
+
+});
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,43 @@
 const express = require('express');
+const mysql = require('mysql');
+const { Connection, Request } = require("tedious");
+
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+    res.render('index', { title: 'The Model Yacht Club' });
+
 });
 
 module.exports = router;
+
+
+/*
+    // Create connection to database
+    const config = {
+        authentication: {
+            options: {
+                userName: "dws_admin", // update me
+                password: "HYgçèuvà!ygç889gbç" // update me
+            },
+            type: "default"
+        },
+        server: "dynawingsail.database.windows.net", // update me
+        options: {
+            database: "user", //update me
+            encrypt: true
+        }
+    };
+
+    const connection = new Connection(config);
+
+    // Attempt to connect and execute queries if connection goes through
+    connection.on("connect", err => {
+        if (err) {
+            console.error(err.code);
+        } else {
+            console.log("OUI");
+        }
+    });  */
