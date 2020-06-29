8365b6be9599165cab6b9b842523cdb4995f4b3f
Charles Castelain <charles.castelain@epitech.eu>
2020-06-29 14:50:51 +0200

    Fonts installed, video integrated

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -13,10 +11,8 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/public', express.static('public'));
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const mysql = require('mysql');
-const { Connection, Request } = require("tedious");
 
 const router = express.Router();
 
@@ -12,32 +10,3 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
-
-
-/*
-    // Create connection to database
-    const config = {
-        authentication: {
-            options: {
-                userName: "dws_admin", // update me
-                password: "HYgçèuvà!ygç889gbç" // update me
-            },
-            type: "default"
-        },
-        server: "dynawingsail.database.windows.net", // update me
-        options: {
-            database: "user", //update me
-            encrypt: true
-        }
-    };
-
-    const connection = new Connection(config);
-
-    // Attempt to connect and execute queries if connection goes through
-    connection.on("connect", err => {
-        if (err) {
-            console.error(err.code);
-        } else {
-            console.log("OUI");
-        }
-    });  */
